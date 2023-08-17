ae6656941c59eda1898fc56ea558641169063bed
panlei-coder <panleiCSDN@163.com>
2023-08-17 17:59:54 +0800

    各种key和comparator

--- db/dbformat.cc
+++ db/dbformat.cc
@@ -13,44 +13,53 @@
 namespace leveldb {
 
 static uint64_t PackSequenceAndType(uint64_t seq, ValueType t) {
+  // seq的最大值为kMaxSequenceNumber
   assert(seq <= kMaxSequenceNumber);
+  // t的最大值为kValueTypeForSeek
   assert(t <= kValueTypeForSeek);
+  // 将sequence与valueType拼接起来,sequence占据高7字节,valueType占据低1字节
   return (seq << 8) | t;
 }
 
+// 将InternalKey添加到result中
 void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
   result->append(key.user_key.data(), key.user_key.size());
   PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
 }
 
+// ParsedInternalKey转换为string
 std::string ParsedInternalKey::DebugString() const {
-  std::ostringstream ss;
+  std::ostringstream ss; // 输出到字符串缓存区
   ss << '\'' << EscapeString(user_key.ToString()) << "' @ " << sequence << " : "
      << static_cast<int>(type);
   return ss.str();
 }
 
+// InternalKey转换为string
 std::string InternalKey::DebugString() const {
   ParsedInternalKey parsed;
   if (ParseInternalKey(rep_, &parsed)) {
     return parsed.DebugString();
   }
-  std::ostringstream ss;
+  std::ostringstream ss; // 输出到字符串缓存区
   ss << "(bad)" << EscapeString(rep_);
   return ss.str();
 }
 
+// 返回比较器的名称
 const char* InternalKeyComparator::Name() const {
   return "leveldb.InternalKeyComparator";
 }
 
+// 比较两个InternalKey
 int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
   // Order by:
   //    increasing user key (according to user-supplied comparator)
   //    decreasing sequence number
   //    decreasing type (though sequence# should be enough to disambiguate)
+  // 首先比较两个user_key_的大小
   int r = user_comparator_->Compare(ExtractUserKey(akey), ExtractUserKey(bkey));
-  if (r == 0) {
+  if (r == 0) { // 如果相等,则比较序列号
     const uint64_t anum = DecodeFixed64(akey.data() + akey.size() - 8);
     const uint64_t bnum = DecodeFixed64(bkey.data() + bkey.size() - 8);
     if (anum > bnum) {
@@ -62,22 +71,29 @@ int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
   return r;
 }
 
+// 寻找start和limit两者之间的最短分隔符
+// 该方法的目的是找到一个最短的字符串start，使得它在逻辑上小于limit，并且在物理上尽可能地与start相似。
 void InternalKeyComparator::FindShortestSeparator(std::string* start,
                                                   const Slice& limit) const {
   // Attempt to shorten the user portion of the key
+  // 尝试缩短键的user部分(提取user_key_)
   Slice user_start = ExtractUserKey(*start);
   Slice user_limit = ExtractUserKey(limit);
+
   std::string tmp(user_start.data(), user_start.size());
   user_comparator_->FindShortestSeparator(&tmp, user_limit);
   if (tmp.size() < user_start.size() &&
       user_comparator_->Compare(user_start, tmp) < 0) {
     // User key has become shorter physically, but larger logically.
     // Tack on the earliest possible number to the shortened user key.
+    // user_key_在物理上变短了，但逻辑上变大了。
+    // 在缩短的user_key_上添加最早可能的数字。
+    // 将最大序列号和用于查找的值类型添加到tmp的末尾
     PutFixed64(&tmp,
                PackSequenceAndType(kMaxSequenceNumber, kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
+    assert(this->Compare(*start, tmp) < 0); // 确保start在tmp之前
+    assert(this->Compare(tmp, limit) < 0); // 确保tmp在limit之前
+    start->swap(tmp); // 交换start和tmp的内容,使得start成为缩短后的字符串
   }
 }
 
@@ -89,6 +105,8 @@ void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
       user_comparator_->Compare(user_key, tmp) < 0) {
     // User key has become shorter physically, but larger logically.
     // Tack on the earliest possible number to the shortened user key.
+    // user_key_在物理上变短了，但逻辑上变大了。
+    // 在缩短的user_key_上添加最早可能的数字。
     PutFixed64(&tmp,
                PackSequenceAndType(kMaxSequenceNumber, kValueTypeForSeek));
     assert(this->Compare(*key, tmp) < 0);
@@ -96,36 +114,49 @@ void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
   }
 }
 
+// 返回policy_name_
 const char* InternalFilterPolicy::Name() const { return user_policy_->Name(); }
 
+// 创建Filter
 void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
                                         std::string* dst) const {
   // We rely on the fact that the code in table.cc does not mind us
   // adjusting keys[].
   Slice* mkey = const_cast<Slice*>(keys);
   for (int i = 0; i < n; i++) {
-    mkey[i] = ExtractUserKey(keys[i]);
+    mkey[i] = ExtractUserKey(keys[i]); // 提取user_key_
     // TODO(sanjay): Suppress dups?
   }
   user_policy_->CreateFilter(keys, n, dst);
 }
 
+// 判断key是否在过滤器f中
 bool InternalFilterPolicy::KeyMayMatch(const Slice& key, const Slice& f) const {
   return user_policy_->KeyMayMatch(ExtractUserKey(key), f);
 }
 
+/*
+key_length(varint32)|user_key(blob)|sequence(7B)|type(1B)|value_length(varint32)|value(blob)
+
+start_**************kstart_******************************end_*******************************
+********************|<----------internal key------------>|**********************************
+********************|<--user key-->|*********************|**********************************
+<--------------------------memtable key----------------->|**********************************
+*/
+
+// 构建LookupKey
 LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
+  size_t usize = user_key.size(); 
+  size_t needed = usize + 13;  // A conservative estimate(保守估计)
   char* dst;
-  if (needed <= sizeof(space_)) {
+  if (needed <= sizeof(space_)) { // 如果所需空间小于等于预分配的空间,则使用预分配空间
     dst = space_;
   } else {
-    dst = new char[needed];
+    dst = new char[needed]; // 否则动态分配
   }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
+  start_ = dst; // 设置起始位置
+  dst = EncodeVarint32(dst, usize + 8); // 将user_key_长度+8编码为varint32,并存放到dst中
+  kstart_ = dst;  // 设置kstart_
   std::memcpy(dst, user_key.data(), usize);
   dst += usize;
   EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
--- util/arena.cc
+++ util/arena.cc
@@ -6,26 +6,31 @@
 
 namespace leveldb {
 
-static const int kBlockSize = 4096;
+static const int kBlockSize = 4096; // 一个块的大小
 
 Arena::Arena()
     : alloc_ptr_(nullptr), alloc_bytes_remaining_(0), memory_usage_(0) {}
 
 Arena::~Arena() {
+  // 释放所有的Block
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
 }
 
+// 按需分配内存，可能会有内存浪费（分配指定大小的内存）
 char* Arena::AllocateFallback(size_t bytes) {
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
+    // Object大于块大小的四分之一。单独分配它，以避免在剩余字节中浪费太多空间。
     char* result = AllocateNewBlock(bytes);
     return result;
   }
 
   // We waste the remaining space in the current block.
+  // 如果需要分配的内存空间大于目前剩余剩余字节数，但是小于1/4的BlockSize，则将剩余字节位置更新
+  // 至新分配的块，原先剩余的部分内存，直接丢弃，目的是快，浪费当前块中的剩余空间。
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
@@ -35,29 +40,38 @@ char* Arena::AllocateFallback(size_t bytes) {
   return result;
 }
 
+// 分配指定大小的内存
 char* Arena::AllocateAligned(size_t bytes) {
+  // 它根据指针的大小来判断应该选择多大的对齐字节数，并且最小为 8。
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
-  static_assert((align & (align - 1)) == 0,
+  // Ensure that the pointer is properly aligned
+  // align为2的幂次时，(align & (align - 1))为0
+  // 因为对齐字节数应该是 2 的幂次，以满足内存对齐要求。
+  static_assert((align & (align - 1)) == 0, 
                 "Pointer size should be a power of 2");
-  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
-  size_t slop = (current_mod == 0 ? 0 : align - current_mod);
-  size_t needed = bytes + slop;
+  // 计算当前分配指针alloc_ptr_的偏移量，以确定当前分配的内存是否满足对齐要求
+  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1); // 计算余数
+  size_t slop = (current_mod == 0? 0 : align - current_mod); // 计算余数基于align的补数
+  // The total number of bytes needed
+  size_t needed = bytes + slop; // 计算需要满足对齐时需要分配的字节数
+
   char* result;
-  if (needed <= alloc_bytes_remaining_) {
+  if (needed <= alloc_bytes_remaining_) { // 判断剩余空间是否满足要求
     result = alloc_ptr_ + slop;
     alloc_ptr_ += needed;
     alloc_bytes_remaining_ -= needed;
-  } else {
+  } else { // 如果剩余字节数不够，就通过AllocateFallback分配
     // AllocateFallback always returned aligned memory
     result = AllocateFallback(bytes);
   }
-  assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
+  assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0); // 判断分配完成之后的内存是对齐的
   return result;
 }
 
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
+  // 已经使用的内存包括被分配的block_bytes，还有指向该block的指针
   memory_usage_.fetch_add(block_bytes + sizeof(char*),
                           std::memory_order_relaxed);
   return result;
